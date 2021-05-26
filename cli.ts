3539993abfe4f23f684d9f85d546b4e0393fd01b
Yoshiya Hinosawa <stibium121@gmail.com>
2021-05-26 22:23:34 +0900

    chore: bump to v0.1.5

--- cli.ts
+++ cli.ts
@@ -3,7 +3,7 @@ import { red } from "https://deno.land/std@0.97.0/fmt/colors.ts";
 import { readDirCreateSource } from "./mod.ts";
 
 const NAME = "deploy_dir";
-const VERSION = "0.1.4";
+const VERSION = "0.1.5";
 
 function usage() {
   console.log(`
